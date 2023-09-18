dfd6680ffec74a3f83b2bcd24969d99b7b28dda1
Abhi <abhigharpankar1722@gmail.com>
2023-09-18 13:49:48 +0530

    testimunoal css added by abhi

--- src/Components/UI/Testimonials.jsx
+++ src/Components/UI/Testimonials.jsx
@@ -1,44 +1,51 @@
 import React from 'react'
+import avatar01 from '../../assets/img/avatar01.png'
+import avatar02 from '../../assets/img/avatar02.png'
+import lunges from '../../assets/img/avatar01.png'
 
 import '../../styles/Testomonials1.css'
 
 const Testimonials = () => {
-  return (
-    <div>
+    return (
+        <div>
+
+            <div className="testimonials">
+                <h1>Testimonials</h1>
+                <p>Lorem ipsum dolor sit amet c repellendus dolorem ut repellat.</p>
+            </div>
 
 
+            <div class="maincontaier">
+                <div class="containerr">
+                    <img src={avatar01}
+                        alt="" />
+                    <h2>Penelope</h2>
+                    <p>Joining FitnessFusion was a life-changing decision! Their expert trainers, state-of-the-art equipment,
+                        and supportive community helped me achieve my fitness goals faster than I ever thought possible</p>
+                </div>
 
-<div class="maincontaier">
-        <div class="container">
-            <img src="https://th.bing.com/th/id/OIP.rgfgEhlfDM4Z-zNAFlDsRgHaKe?pid=ImgDet&w=176&h=248&c=7&dpr=1.7"
-                alt=""/>
-            <h2>Penelope</h2>
-            <p>Joining FitnessFusion was a life-changing decision! Their expert trainers, state-of-the-art equipment,
-                and supportive community helped me achieve my fitness goals faster than I ever thought possible</p>
+
+                <div class="containerr">
+                    <img src={avatar02}
+                        alt="" />
+                    <h2>Beatrice</h2>
+                    <p>I've tried many gyms, but FitLife Gym is truly exceptional. The personalized training plans, motivating
+                        atmosphere, and visible results have kept me coming back for years.
+                    </p>
+                </div>
+
+
+                <div class="containerr">
+                    <img src={lunges}
+                        alt="" />
+                    <h2>Josephine</h2>
+                    <p>FitnessPro Gym is the real deal! Thanks to their professional coaches and diverse workout options, I've
+                        never been in better shape. I can't recommend this gym enough!
+                    </p>
+                </div>
+            </div>
         </div>
-
-
-        <div class="container">
-            <img src="https://th.bing.com/th/id/OIP.yvPfENddrAbg5ZdgEfcpogAAAA?pid=ImgDet&w=177&h=228&c=7&dpr=1.7"
-                alt=""/>
-            <h2>Beatrice</h2>
-            <p>I've tried many gyms, but FitLife Gym is truly exceptional. The personalized training plans, motivating
-                atmosphere, and visible results have kept me coming back for years.
-            </p>
-        </div>
-
-
-        <div class="container">
-            <img src="https://th.bing.com/th/id/OIP.Eh3dnAjqa1k1GPHcW1KvggHaE8?w=273&h=182&c=7&r=0&o=5&dpr=1.7&pid=1.7"
-                alt=""/>
-            <h2>Josephine</h2>
-            <p>FitnessPro Gym is the real deal! Thanks to their professional coaches and diverse workout options, I've
-                never been in better shape. I can't recommend this gym enough!
-            </p>
-        </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default Testimonials
